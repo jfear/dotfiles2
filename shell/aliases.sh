38b28e48b5d2e91b2fc9fe5bf29f90512bb3e36e
Justin Fear <justin.m.fear@gmail.com>
2022-02-20 16:27:30 -0500

    remove old aliases that I don't use anymore

--- shell/aliases.sh
+++ shell/aliases.sh
@@ -26,32 +26,6 @@ alias htopu='htop -u fearjm'
 [[ -f ~/.local/bin/bat ]] && alias cat="bat"
 [[ -f ~/.local/bin/dust ]] && alias du="dust"
 
-# Git
-alias gs='git status'
-alias ga='git add'
-alias gb='git branch'
-alias gc='git commit'
-alias gd='git diff'
-alias go='git checkout'
-alias gps='git push'
-alias gpl='git pull'
-alias gl='git log'
-alias gh='git log --pretty=format:"%h %ad | %s%d [%an]" --graph --date=short'
-alias glol='git log --graph --decorate --pretty=oneline --abbrev-commit --all'
-alias gl2='git log --pretty=format:"%h %s [%an]" --graph'
-
-# Slurm
-if [ ! -z "$LMOD_sys" ]; then
-  alias sme='sjobs --no-column-limit -u fearjm'
-  alias wsme='watch -n 120 "sjobs --no-column-limit -u fearjm"'
-  alias si='sinteractive --mem=12g --cpus-per-task=4 --time=12:00:00 --gres=lscratch:20'
-
-  alias ms='module spider'
-  alias ml='module load'
-  alias mi='module show'
-fi
-
-
 # vim
 if hash nvim 2>/dev/null; then
   alias vi='nvim'
